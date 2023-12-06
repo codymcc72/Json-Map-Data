f93dbfc7d202a26f8c7776bf6d96b178c7cef966
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-12-05 23:41:40 -0800

    Update json_mapping_ideal_times.py

--- json_mapping_ideal_times.py
+++ json_mapping_ideal_times.py
@@ -1,44 +1,25 @@
-'''
-
-JsonDataMap Class:
-
-The JsonDataMap class ectracts relevant information from the JSON data and visualizing it using the Matplotlib library. 
-
-1. Data Extraction:
-   - The class has methods to extract rows, turns, start paths, end paths, and the datum from the given JSON data. These methods adjust the coordinates of the points based on the map's datum.
-   - Rows represent treatment areas, and the extracted data includes x and y coordinates.
-
-2. Plotting Data:
-   - The `plot_data` method uses Matplotlib to create a scatter plot of the extracted data. Different colors are used for rows, turns, start paths, end paths, and the datum.
-   - Labels, legend, and axes information are added to enhance the clarity of the plot.
-
-IdealTime Class:
-
-The `IdealTime` class focuses on calculating and storing ideal travel times based on the extracted rows. Here's a detailed explanation of its functionalities:
-
-1. Distance Calculation:
-   - The `calculate_distance` method calculates the Euclidean distance between two points. This is used to determine the total distance traveled in each row.
-
-2. Travel Time Calculation:
-   - The `calculate_and_store_travel_times` method processes the rows from the JSON data and calculates the total distance traveled in each row.
-   - The total distance is then divided by the specified speed to obtain the travel time in seconds.
-   - The calculated travel times are stored in the `self.ideal_travel_times` list.
-
-
-'''
-
 import json
 import math
 import matplotlib.pyplot as plt
 
+# Class to represent map data from JSON
 class JsonDataMap:
     def __init__(self):
+        # Initialize attributes to store map segments
         self.rows = None
         self.turns = None
         self.start_path = None
         self.end_path = None
         self.datum = None
 
+    # Helper method to calculate distance between two points
+    def calculate_distance(self, point1, point2):
+        x1, y1, z1 = point1['head']['position']['x'], point1['head']['position']['y'], point1['head']['position']['z']
+        x2, y2, z2 = point2['head']['position']['x'], point2['head']['position']['y'], point2['head']['position']['z']
+        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        return distance
+
+    # Extract rows data from JSON
     def extract_rows(self, json_data):
         x = [point['head']['position']['x'] + json_data['datum']['longitude'] for point in json_data['points']]
         y = [point['head']['position']['y'] + json_data['datum']['latitude'] for point in json_data['points']]
@@ -56,8 +37,12 @@ class JsonDataMap:
         rows_x = [point['x'] for point in rows]
         rows_y = [point['y'] for point in rows]
 
-        return {'x': rows_x, 'y': rows_y}
+        # Calculate total distance for rows
+        total_distance = sum(math.sqrt((rows_x[i+1] - rows_x[i])**2 + (rows_y[i+1] - rows_y[i])**2) for i in range(len(rows_x)-1))
+
+        return {'x': rows_x, 'y': rows_y, 'total_distance': total_distance}
     
+    # Extract turns data from JSON
     def extract_turns(self, json_data):
         x = [point['head']['position']['x'] + json_data['datum']['longitude'] for point in json_data['points']]
         y = [point['head']['position']['y'] + json_data['datum']['latitude'] for point in json_data['points']]
@@ -78,8 +63,12 @@ class JsonDataMap:
         turns_x = [point['x'] for point in turns]
         turns_y = [point['y'] for point in turns]
 
-        return {'x': turns_x, 'y': turns_y}
+        # Calculate total distance for turns
+        total_distance = sum(math.sqrt((turns_x[i+1] - turns_x[i])**2 + (turns_y[i+1] - turns_y[i])**2) for i in range(len(turns_x)-1))
 
+        return {'x': turns_x, 'y': turns_y, 'total_distance': total_distance}
+
+    # Extract start path data from JSON
     def extract_start_path(self, json_data):
         x = [point['head']['position']['x'] + json_data['datum']['longitude'] for point in json_data['points']]
         y = [point['head']['position']['y'] + json_data['datum']['latitude'] for point in json_data['points']]
@@ -91,8 +80,12 @@ class JsonDataMap:
         start_path_x = x[:first_treatment_area_index]
         start_path_y = y[:first_treatment_area_index]
 
-        return {'x': start_path_x, 'y': start_path_y}
+        # Calculate total distance for start path
+        total_distance = sum(math.sqrt((start_path_x[i+1] - start_path_x[i])**2 + (start_path_y[i+1] - start_path_y[i])**2) for i in range(len(start_path_x)-1))
 
+        return {'x': start_path_x, 'y': start_path_y, 'total_distance': total_distance}
+
+    # Extract end path data from JSON
     def extract_end_path(self, json_data):
         x = [point['head']['position']['x'] + json_data['datum']['longitude'] for point in json_data['points']]
         y = [point['head']['position']['y'] + json_data['datum']['latitude'] for point in json_data['points']]
@@ -107,62 +100,66 @@ class JsonDataMap:
         end_path_x = x[last_treatment_area_index:]
         end_path_y = y[last_treatment_area_index:]
 
-        return {'x': end_path_x, 'y': end_path_y}
+        # Calculate total distance for end path
+        total_distance = sum(math.sqrt((end_path_x[i+1] - end_path_x[i])**2 + (end_path_y[i+1] - end_path_y[i])**2) for i in range(len(end_path_x)-1))
 
+        return {'x': end_path_x, 'y': end_path_y, 'total_distance': total_distance}
+
+    # Extract datum (home) coordinates from JSON
     def extract_datum(self, json):
         x = json['datum']['longitude']
         y = json['datum']['latitude']
         return {'x': x, 'y': y}
 
+    # Plot the extracted map data
     def plot_data(self):
-        #rows
-        plt.scatter(self.rows['x'], 
-                    self.rows['y'], 
-                    color='mediumseagreen', 
-                    label='Rows', 
-                    s=10)
-        
-        #turns
-        plt.scatter(self.turns['x'], 
-                    self.turns['y'], 
-                    color='lightsteelblue', 
-                    label='Turns', 
-                    s=10)
-        
-        #start path
-        plt.scatter(self.start_path['x'], 
-                    self.start_path['y'], 
-                    color='steelblue', 
-                    label='Start Path', 
-                    s=10)
-        
-        #end path
-        plt.scatter(self.end_path['x'], 
-                    self.end_path['y'], 
-                    color='tomato', 
-                    label='End Path', 
+        # Scatter plot for rows
+        plt.scatter(self.rows['x'],
+                    self.rows['y'],
+                    color='mediumseagreen',
+                    label='Rows',
                     s=10)
 
-        # Plotting the datum
-        plt.scatter(self.datum['x'], 
-                    self.datum['y'], 
-                    color='black', 
-                    marker='x', 
+        # Scatter plot for turns
+        plt.scatter(self.turns['x'],
+                    self.turns['y'],
+                    color='lightsteelblue',
+                    label='Turns',
+                    s=10)
+
+        # Scatter plot for start path
+        plt.scatter(self.start_path['x'],
+                    self.start_path['y'],
+                    color='steelblue',
+                    label='Start Path',
+                    s=10)
+
+        # Scatter plot for end path
+        plt.scatter(self.end_path['x'],
+                    self.end_path['y'],
+                    color='tomato',
+                    label='End Path',
+                    s=10)
+
+        # Scatter plot for datum (home)
+        plt.scatter(self.datum['x'],
+                    self.datum['y'],
+                    color='black',
+                    marker='x',
                     label='Home')
 
-        # Adding labels
-       
-
+        # Add labels
         plt.xlabel('X Label')
         plt.ylabel('Y Label')
 
-        # Adding a legend
+        # Add legend
         plt.legend()
 
         # Show the plot
         plt.show()
 
 
+# Class to calculate and store ideal travel times
 class IdealTime:
     def __init__(self, json_data, speed):
         self.json_data = json_data
@@ -170,12 +167,14 @@ class IdealTime:
         self.ideal_travel_times = []  # List to store ideal travel times
         self.total_distance_rows = 0  # Variable to store the total distance of all rows
 
+    # Helper method to calculate distance between two points
     def calculate_distance(self, point1, point2):
         x1, y1, z1 = point1['head']['position']['x'], point1['head']['position']['y'], point1['head']['position']['z']
         x2, y2, z2 = point2['head']['position']['x'], point2['head']['position']['y'], point2['head']['position']['z']
         distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
         return distance
 
+    # Calculate and store ideal travel times for each row
     def calculate_and_store_travel_times(self):
         # Extracting relevant information
         points = self.json_data['points']
@@ -212,22 +211,24 @@ class IdealTime:
         for i, row in enumerate(rows):
             total_distance = sum(self.calculate_distance(row[j], row[j + 1]) for j in range(len(row) - 1))
             travel_time_seconds = total_distance / self.speed/60  # Travel time in seconds/60 to make minutes
-            rounded_travel_time_minutes = round(travel_time_seconds, 1)  # Round to 1 decimal place
+            rounded_travel_time_minutes = round(travel_time_seconds, 2)  # Round to 1 decimal place
             self.ideal_travel_times.append(rounded_travel_time_minutes)
 
-            # print(f"Row {i + 1}: Ideal Travel Time = {travel_time_minutes:.2f} minutes")
-
+    # Getter method to retrieve total distance of all rows
     def get_total_distance_rows(self):
         return self.total_distance_rows
 
+    # Getter method to retrieve ideal travel times
     def get_ideal_travel_times(self):
         return self.ideal_travel_times
 
 
+# Load JSON data from file
 def load_json_data(file_name):
     with open(file_name, 'r') as file:
         return json.load(file)
 
+# Process and plot map data
 def process_and_plot_data(json_data, speed_treatment, speed_non_treatment):
     plot_data = JsonDataMap()
     ideal_time = IdealTime(json_data, speed_treatment)
@@ -239,34 +240,26 @@ def process_and_plot_data(json_data, speed_treatment, speed_non_treatment):
     plot_data.end_path = plot_data.extract_end_path(json_data)
     plot_data.datum = plot_data.extract_datum(json_data)
 
-    # Plot the data
-    plot_data.plot_data()
+    # Access the total distance for each segment
+    total_distance_rows = plot_data.rows['total_distance']
+    total_distance_turns = plot_data.turns['total_distance']
+    total_distance_start_path = plot_data.start_path['total_distance']
+    total_distance_end_path = plot_data.end_path['total_distance']
+
+    # Print or use the total distance information as needed
+    print("Total Distance for Rows:", total_distance_rows, "meters")
+    print("Total Distance for Turns:", total_distance_turns, "meters")
+    print("Total Distance for Start Path:", total_distance_start_path, "meters")
+    print("Total Distance for End Path:", total_distance_end_path, "meters")
 
     # Calculate, print, and store ideal travel times for treatment areas
     ideal_time.calculate_and_store_travel_times()
 
     # Access the stored ideal travel times for future use
     stored_ideal_times_treatment = ideal_time.get_ideal_travel_times()
-    print("Stored Ideal Travel Times for Treatment Areas:", stored_ideal_times_treatment)
+    print("Stored Ideal Travel Times for Rows in Minutes:", stored_ideal_times_treatment)
 
-    return plot_data, ideal_time
-
-def calculate_and_print_ideal_travel_time(total_distance_rows, speed, label):
-    ideal_travel_time = total_distance_rows / speed
-    rounded_ideal_travel_time = round(ideal_travel_time / 3600, 3)
-    print(f"Ideal Travel Time for {label}:", rounded_ideal_travel_time, " Hours")
-
-def main():
-    # Load JSON data
-    file_name = 'appended.json'
-    json_data = load_json_data(file_name)
-
-    # Set speeds for ideal time calculation (in meters per second)
-    speed_treatment = 0.23
-    speed_non_treatment = 0.5
-
-    plot_data, ideal_time = process_and_plot_data(json_data, speed_treatment, speed_non_treatment)
-
+    
     # Access the total distance of rows for treatment areas
     total_distance_rows_treatment = ideal_time.get_total_distance_rows()
 
@@ -280,6 +273,29 @@ def main():
     # Calculate and print ideal travel time for non-treatment areas
     total_distance_non_treatment = total_distance_all_points - total_distance_rows_treatment
     calculate_and_print_ideal_travel_time(total_distance_non_treatment, speed_non_treatment, "Non-Treatment Areas")
+
+    # Plot the data
+    plot_data.plot_data()
+
+    return plot_data, ideal_time
+
+# Calculate and print ideal travel time for a given segment
+def calculate_and_print_ideal_travel_time(total_distance, speed, label):
+    ideal_travel_time = total_distance / speed
+    rounded_ideal_travel_time = round(ideal_travel_time / 60, 2)
+    print(f"Ideal Travel Time for {label}:", rounded_ideal_travel_time, " Minutes")
+
+# Main function
+def main():
+    # Load JSON data
+    file_name = 'test_map_124.json'
+    json_data = load_json_data(file_name)
+
+    # Set speeds for ideal time calculation (in meters per second)
+    speed_treatment = 0.23
+    speed_non_treatment = 0.5
+
+    plot_data, ideal_time = process_and_plot_data(json_data, speed_treatment, speed_non_treatment)
 
 if __name__ == "__main__":
     main()

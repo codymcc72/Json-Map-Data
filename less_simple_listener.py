3d6070ac5cb73dec4c5c99687ae45e433344c68f
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-12-07 07:37:56 -0800

    Update less_simple_listener.py

--- less_simple_listener.py
+++ less_simple_listener.py
@@ -19,31 +19,34 @@ class JsonDataMap:
         adjusted_y = point['head']['position']['y'] + self.datum['latitude']
         return {'x': adjusted_x, 'y': adjusted_y}
 
-def gps_callback(gps_data, json_data_map, passed_point):
-    # Iterate through map points to find the closest one
+def gps_callback(gps_data, json_data_map, radius, passed_point):
+    # GPS coordinates
     x_gps = gps_data.longitude
     y_gps = gps_data.latitude
 
-    closest_point = None
-    min_distance = float('inf')
-
+    # Iterate through map points to find if GPS coordinates are close to any point
     for point in json_data_map.points:
         adjusted_point = json_data_map.adjust_coordinates(point)
+        
+        # Calculate distance between GPS coordinates and adjusted point
         distance = math.sqrt((x_gps - adjusted_point['x'])**2 + (y_gps - adjusted_point['y'])**2)
 
-        if distance < min_distance:
-            min_distance = distance
-            closest_point = adjusted_point
+        # Check if GPS coordinates are within the specified radius of the point
+        if distance < radius:
+            # Check if the point has not been passed recently
+            if passed_point is None or adjusted_point != passed_point['point']:
+                passed_point = {'point': adjusted_point, 'distance': distance}
+                timestamp = gps_data.header.stamp
 
-    # Check if the closest point has been passed
-    if passed_point is None or min_distance < passed_point['distance']:
-        passed_point = {'point': closest_point, 'distance': min_distance}
-        timestamp = gps_data.header.stamp
+                print("Adjusted X: {:.6f}".format(adjusted_point['x']))
+                print("Adjusted Y: {:.6f}".format(adjusted_point['y']))
+                print("Timestamp: {}".format(timestamp))
+                print("\n")
 
-        print("Adjusted X: {:.6f}".format(closest_point['x']))
-        print("Adjusted Y: {:.6f}".format(closest_point['y']))
-        print("Timestamp: {}".format(timestamp))
-        print("\n")
+                # You may want to add additional logic or processing here
+
+            break  # Exit the loop once a matching point is found
+
 
     return passed_point
 

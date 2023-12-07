5aeb9ec8a12008486e7a76306221a4413643d592
codymcc72 <151500314+codymcc72@users.noreply.github.com>
2023-12-07 07:25:12 -0800

    Update less_simple_listener.py

--- less_simple_listener.py
+++ less_simple_listener.py
@@ -8,24 +8,18 @@ import math
 
 # Class to represent map data from JSON
 class JsonDataMap:
-    def __init__(self):
+    def __init__(self, json_data):
         # Initialize attributes to store map segments
-        self.datum = None
-        self.points = None
-
-    # Extract datum (home) coordinates from JSON
-    def extract_datum(self):
-        x = self.datum['longitude']
-        y = self.datum['latitude']
-        return {'x': x, 'y': y}
+        self.datum = json_data['datum']
+        self.points = json_data['points']
 
     # Adjust coordinates using the datum information
     def adjust_coordinates(self, point):
-        adjusted_x = point['head']['position']['x'] + self.datum['x']
-        adjusted_y = point['head']['position']['y'] + self.datum['y']
+        adjusted_x = point['head']['position']['x'] + self.datum['longitude']
+        adjusted_y = point['head']['position']['y'] + self.datum['latitude']
         return {'x': adjusted_x, 'y': adjusted_y}
 
-def gps_callback(gps_data, json_data, datum, passed_point):
+def gps_callback(gps_data, json_data_map, passed_point):
     # Iterate through map points to find the closest one
     x_gps = gps_data.longitude
     y_gps = gps_data.latitude
@@ -33,8 +27,8 @@ def gps_callback(gps_data, json_data, datum, passed_point):
     closest_point = None
     min_distance = float('inf')
 
-    for point in json_data.points:
-        adjusted_point = json_data.adjust_coordinates(point)
+    for point in json_data_map.points:
+        adjusted_point = json_data_map.adjust_coordinates(point)
         distance = math.sqrt((x_gps - adjusted_point['x'])**2 + (y_gps - adjusted_point['y'])**2)
 
         if distance < min_distance:
@@ -53,16 +47,13 @@ def gps_callback(gps_data, json_data, datum, passed_point):
 
     return passed_point
 
-def gps_listener(json_data):
+def gps_listener(json_data_map):
     rospy.init_node('gps_listener', anonymous=True)
-
-    # Extract datum information from JSON
-    datum = json_data.extract_datum()
 
     # Variable to track the point that has been passed
     passed_point = None
 
-    rospy.Subscriber('tric_navigation/gps/head_data', NavSatFix, lambda gps_data: gps_callback(gps_data, json_data, datum, passed_point))
+    rospy.Subscriber('tric_navigation/gps/head_data', NavSatFix, lambda gps_data: gps_callback(gps_data, json_data_map, passed_point))
     rospy.spin()
 
 if __name__ == '__main__':
@@ -75,9 +66,7 @@ if __name__ == '__main__':
         json_data = json.load(file)
 
     # Initialize JsonDataMap with the loaded JSON data
-    json_data_map = JsonDataMap()
-    json_data_map.datum = json_data_map.extract_datum(json_data)
-    json_data_map.points = json_data['points']
+    json_data_map = JsonDataMap(json_data)
 
     # Initialize GPS listener with the JsonDataMap instance
     gps_listener(json_data_map)
